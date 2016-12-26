34b01012195d0a1cf61f6a5fbe9a33cf28646682
Valentin Encinas <vencinas@valentinencinas.es>
2016-12-26 12:33:21 +0100

    Maven Project Spring Boot basic: "Hello World!!!"

---
+++ src/main/java/es/vencinas/examples/spring/boot/HelloWordSpringBootApplication.java
@@ -0,0 +1,12 @@
+package es.vencinas.examples.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWordSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWordSpringBootApplication.class, args);
+	}
+}
